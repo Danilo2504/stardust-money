0a1e70615b2dfa4d765476d794f2d706cd455a99
Danilo2504 <danilobautista2004@gmail.com>
2026-06-28 20:44:34 +0200

    ui: enlarge form modals to modal-lg

--- resources/views/pages/recurring-expenses/form.blade.php
+++ resources/views/pages/recurring-expenses/form.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="recurringExpenseModal" tabindex="-1" aria-labelledby="recurringExpenseModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="recurringExpenseModalLabel">
